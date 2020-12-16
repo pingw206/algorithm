047745306c46d4760b9b7dd393ffdef0ab601a51
pingw206 <pingw206@gmail.com>
2020-12-16 22:59:20 +0800

    update

--- leetcode/S0203_RemoveLinkdListElements.js
+++ leetcode/S0203_RemoveLinkdListElements.js
@@ -1,6 +1,6 @@
 //我自己20分钟一遍过哦～ 开始卡在了最后一个节点要不要单独处理上，后来发现循环时已经处理了呀：）
 //宝子还有一种投机取巧的方法，就是新建一个链表，把符合的数放进去，不符合的数不放，不推荐
-
+//2020-12-16更新，没做出来的原因是想错了链表指针的跳转，把else 里面的话放到了平行地方，以为要跳转两次；其实跳转pointer.next 和跳转pointer是一样的
 var removeElements = function(head, val) {
   var headNode = new ListNode(0);
   headNode.next = head;

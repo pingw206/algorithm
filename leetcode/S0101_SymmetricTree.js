d71f7e2ee794753a85e3606ab5413fc414963488
pingw206 <pingw206@gmail.com>
2020-12-23 22:06:55 +0800

    update

--- leetcode/S0101_SymmetricTree.js
+++ leetcode/S0101_SymmetricTree.js
@@ -1,3 +1,4 @@
+//难点在于想清楚第三层往下是什么样的规律 
 var isSymmetric1 = function(root) {
     if (root == null) {
         return true;
@@ -14,7 +15,7 @@ var isTwoTreesSymmetric = function(root1, root2) {
     }
     return root1.val == root2.val &&
         isTwoTreesSymmetric(root1.left, root2.right) &&
-        isTwoTreesSymmetric(root1.right, root2.left);
+        isTwoTreesSymmetric(root1.right, root2.left); //这里不能写成if(...)return true; 因为会丢掉很多为false的情况
 }
 
 //做完100题，再想想这道题就会啦， 小心绕哦～

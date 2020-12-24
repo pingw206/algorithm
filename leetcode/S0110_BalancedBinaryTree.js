185c0fece8b87d08d96df727b192bbdd15bb3819
pingw206 <pingw206@gmail.com>
2020-12-24 21:57:26 +0800

    update

--- leetcode/S0110_BalancedBinaryTree.js
+++ leetcode/S0110_BalancedBinaryTree.js
@@ -1,5 +1,8 @@
 //最开始想到的办法，最小子问题是root == null; 原问题和子问题的关系：子问题的两个子树是平衡二叉树，且root的两个子树的高度差 <= 1; 
 //时间复杂度是n*2,因为计算isBalanced递归一遍，计算height又要递归调用一遍，涉及到多次重复计算
+
+//2020-12-24 想到了第一递归平衡树，第二求高度，所以是两层递归，但没有写出来高度的函数，和最大深度是一样的，求树的深度是基本要求，还没有掌握
+//注意是调用isBalanced本身，不需要再写一个isTwoBalanced(root.left, root.right),读懂题目要求，左右两个树都要是平衡树，不是组合在一起
 var isBalanced = function(root) {
     if (root == null) {
         return true;

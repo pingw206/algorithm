dd6adfc822c666429265c10b8edd2e5c7c981b75
pingw206 <pingw206@gmail.com>
2020-12-26 21:48:25 +0800

    update

--- leetcode/S0098_ValidateBinarySearchTree.js
+++ leetcode/S0098_ValidateBinarySearchTree.js
@@ -1,4 +1,8 @@
-//还是不太懂， 关键是放置左右的min, max ；别忘等号
+//还是不太懂， 关键是放置左右的min, max， 当左节点是root时，比他大的就是root；当右节点是root时，比他小的就是root ；
+//别忘等号
+
+//2020-12-24 理解题意，不是说左节点比中间的节点小，而是说所有的左子树的节点都比中间节点小，
+//所以[5,4,6,null,null,3,7] 是false
 var isValidBST = function(root) {
     return internalIsValidBST(root, null, null);
 };

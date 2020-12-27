5b9dd60749e3f5e9538721e1a874b470ea3e6765
pingw206 <pingw206@gmail.com>
2020-12-27 17:22:28 +0800

    update

--- leetcode/S0206_ReverseLinkedList.js
+++ leetcode/S0206_ReverseLinkedList.js
@@ -1,12 +1,20 @@
 // iteratively or recursively 循环或递归两种方法实现 
+
 //1.循环: 用头插法，先创造一个新链表， 0， 然后往前面循环依次插入 1-2-3-4-5， 变成 0-1；0-2-1；0-3-2-1； 最后返回0.next;
 //newList一位置永远不动，一直在0那里
+
+/*2020-12-27 
+头插法方法知道，但是如何处理head那个节点，不被当前反转节点影响，后续关联很重要；
+所以要弄成清晰的两步走：1st. 取出来head节点，head继续跳转下一个（取出来的节点根据方法看是否需要处理成单节点）
+2nd.头插法
+*/
+
 var reverseList = function(head) {
   var newList = new ListNode(0);
   while (head !== null) {
-      var headNode = head;
+      var headNode = head;//取出来head放到新节点中，不再影响head
       head = head.next;//断开联系，赶紧更新head,以免被headNode影响
-      headNode.next = null; //跟head断开,这一步在这个题里不是很重要，但是操作到最后是最后一个节点就很必要，不然后面还带着一长串
+      headNode.next = null; //跟head断开,这一步操作到最后一个节点很必要，不然后面还带着一长串
       
       //接下来用头插法append to head
       var nextNode = newList.next;
@@ -19,8 +27,8 @@ var reverseList = function(head) {
 var reverseList = function(head) {
   var newList = new ListNode(0);
   while (head !== null) {
-      var headNode = head;
-      head = head.next;
+      var headNode = head;//取出来head放到新节点中，不再影响head
+      head = head.next; //让head跳到下一个
       //先接后面，再接前面：
       headNode.next = newList.next;
       newList.next = headNode;
@@ -30,6 +38,7 @@ var reverseList = function(head) {
 };
 
 //2.递归
+//2020-12-27 没想到f(n-1)是什么，是head.next
 /*递归有三步：1，最小值时的返回值；2，已知f(n-1)时，如何计算f(n) 3,计算f(n)后返回值是什么 
 */
 var reverseList = function(head) {

d80f8505c09c98a829569f704c555f51bb4a4a17
pingw206 <pingw206@gmail.com>
2020-12-29 21:45:39 +0800

    update

--- leetcode/S0020_ValidParentheses.js
+++ leetcode/S0020_ValidParentheses.js
@@ -1,3 +1,37 @@
+/*2020-12-28 想用栈，但是想不到f(n-1)是什么，栈又不是递归，非要什么f(n-1)!! */
+//优化写法， 首先判断条件可以优化到两条，然后是把符号都先存到map里面，再使用；
+
+/*Map知识点:Map 与数组的关系
+let kvArray = [["key1", "value1"], ["key2", "value2"]];
+let myMap = new Map(kvArray);// Map构造函数可以将一个二维键值对数组转换成一个Map对象
+myMap.get("key1"); // 返回值为 "value1"
+
+Map.prototype.get(key)
+返回键对应的值，如果不存在，则返回undefined。
+Map.prototype.has(key)
+返回一个布尔值，表示Map实例是否包含键对应的值。
+ */
+var isValid = function(s) {
+    let kvArray = [[')', '('], ['}', '{'], [']', '[']];  //注意存的是反的，为了调用右半边当keykey
+    var matchMap = new Map(kvArray);
+    var newStack = new Array();
+    //多层if else的嵌套要了解原理
+    for (var i = 0; i < s.length; i++) {
+        if (!matchMap.has(s[i])) {    //逐个分析遇到的key是否为右半边，不是话，就是新出现的左半边，push进去
+            newStack.push(s[i]);
+        } else {
+            if (newStack.length == 0 || newStack[newStack.length - 1] != matchMap.get(s[i])) {
+                return false;   //是右半边时，如果左边没有符号来匹配，或者左边的符号和它不匹配,一定是在length-1的位置上匹配（先考虑length == 0的情况，然后进入length-1也就有意义了）
+            } else {
+                newStack.pop();
+            }
+        }
+    }
+    if (newStack.length == 0) {
+        return true;
+    }
+    return false;
+};
 //我最开始的写法，遍历一遍所有的符号（其实S本身就是字符串，可以定位到第几个字符的，不用split)
 var isValid = function(s) {
     var sList = s.split("");
@@ -41,25 +75,3 @@ var isValid = function(s) {
     return false;
 };
 
-//优化写法， 首先判断条件可以优化到两条，然后是把符号都先存到map里面，再使用；注意存的是反的，为了调key
-var isValid = function(s) {
-    let kvArray = [[')', '('], ['}', '{'], [']', '[']];
-    var matchMap = new Map(kvArray);
-    var newStack = new Array();
-    
-    for (var i = 0; i < s.length; i++) {
-        if (!matchMap.has(s[i])) {    //不是key,也就是说不是右半边，即是左半边，push进去
-            newStack.push(s[i]);
-        } else {
-            if (newStack.length == 0 || newStack[newStack.length - 1] != matchMap.get(s[i])) {
-                return false;   //是右半边时，如果左边没有符号来匹配，或者左边的符号和它不匹配（先考虑length == 0的情况，然后进入length-1也就有意义了）
-            } else {
-                newStack.pop();
-            }
-        }
-    }
-    if (newStack.length == 0) {
-        return true;
-    }
-    return false;
-};
--- leetcode/S0155_MinStack.js
+++ leetcode/S0155_MinStack.js
@@ -1,13 +1,7 @@
 /**
  * 1.这类构造题没想到首先应该怎么写，两个this
- * 2.push， minStack的时候，忘了加小于等于的等于号，就错啦，想一想为什么错
+ * 2.push， minStack的时候，忘了加小于等于的等于号，就错啦，如果[0,-1,0],minStack应该是[0,0],不然[0]一次pop就没了
  * 3.pop的时候可以加判断条件是否有意义，不过这道题是默认有意义的，也可以不加
- * 4.pop那步张磊的写法
- * MinStack.prototype.pop = function() {
-    var popVal = this.stack.pop();
-    if (popVal == this.minStack[this.minStack.length-1]) {
-        this.minStack.pop();
-    }
 };
  */
 var MinStack = function() {
@@ -33,12 +27,10 @@ MinStack.prototype.pop = function() {
     if (this.stack.length == 0) {
         return;
     }
-    if (this.stack[this.stack.length - 1] == this.minStack[this.minStack.length - 1]) {
-        this.stack.pop();
+    var popVal = this.stack.pop();
+    if (popVal == this.minStack[this.minStack.length-1]) {
         this.minStack.pop();
-    } else {
-        this.stack.pop();
-    }  
+    } 
 };
 
 /**

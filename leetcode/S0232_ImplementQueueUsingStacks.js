3f01b874f66bf0a6e2a115d29639764bb02d45b4
pingw206 <pingw206@gmail.com>
2020-12-29 22:41:09 +0800

    update

--- leetcode/S0232_ImplementQueueUsingStacks.js
+++ leetcode/S0232_ImplementQueueUsingStacks.js
@@ -1,5 +1,6 @@
 //用stack 实现queue
-/*本解法是在peek()中来倒置一下列表，因为peek()没有操作产生，然后pop调用peek实际上就是为了倒置一下，也可以写一个公共的倒置函数供peek,pop用
+/*本解法的关键是用两个栈，第一个数组用来存push，第二个用来倒置成需要的队列-先进先出
+是在peek()中来倒置一下列表，因为peek()没有操作产生，然后pop调用peek实际上就是为了倒置一下，也可以写一个公共的倒置函数供peek,pop用
 stack1  stack2
  3         1
  2         2 
@@ -27,8 +28,10 @@ MyQueue.prototype.push = function(x) {
  * @return {number}
  */
 MyQueue.prototype.pop = function() {
-    if (this.empty()) { /* throw exception,本题设定没有异常情况，可以不写此句话 */}
-    var popVal = this.peek();  /*!!!这句话非常关键！！！不然就不会存在stack2 */
+    if (this.empty()) { 
+        return;
+    }//本题设定没有异常情况，可以不写此句话 
+    var popVal = this.peek();  /*!!!这句话非常关键！！！调用peek()方法生成stack2 */
     this.stack2.pop();
     return popVal;
 };
@@ -38,8 +41,10 @@ MyQueue.prototype.pop = function() {
  * @return {number}
  */
 MyQueue.prototype.peek = function() {
-    if (this.empty()) { /* throw exception */}  
-    if (this.stack2.length == 0) {   /**这个判断别忘了 */
+    if (this.empty()) { 
+        return;
+    }//本题设定没有异常情况，可以不写此句话 
+    if (this.stack2.length == 0) {   /**这个判断很重要，调用的过程遇到stack2中的数用完了，要随时从又push了的stack1中补充；还没用完的话因为先进先出，还有的可以出，可以等全部用完再补充 */
         while (!this.stack1.length == 0) {
             this.stack2.push(this.stack1[this.stack1.length-1]);
             this.stack1.pop();
@@ -58,7 +63,7 @@ MyQueue.prototype.empty = function() {
 
 
 
-
+/*我的错误第一想法 */
 // 我嗖嗖嗖10分钟用array实现了queue，没仔细想人家是让用stack的功能实现queue
 var MyQueue = function() {
     this.queue = new Array();

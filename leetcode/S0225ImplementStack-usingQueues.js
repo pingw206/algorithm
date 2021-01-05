04a97a5ae935a540c400d8a0dcfd09b78f7f8295
pingw206 <pingw206@gmail.com>
2021-01-05 21:47:40 +0800

    update

--- leetcode/S0225ImplementStack-usingQueues.js
+++ leetcode/S0225ImplementStack-usingQueues.js
@@ -1,6 +1,61 @@
 // 关键难点是想通了push的时候怎么做，后面就很简单了，切忌照搬232题
+//两种方法，用一个或者两个队列都可以实现
+
+/*方法一 用两个queue实现 */
+//用两个数组做成的队列，只能有队列的操作，也就是shift()从头上弹出,push()从尾巴加入
+/**
+ * Initialize your data structure here.
+ */
+var MyStack = function() {
+    this.queue1 = [];
+    this.queue2 = [];
+};
 
 /**
+ * Push element x onto stack.
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.queue1.push(x);
+};
+
+/**
+ * Removes the element on top of the stack and returns that element.
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    while (this.queue1.length > 1) {
+        this.queue2.push(this.queue1.shift());
+    }
+    var popValue = this.queue1.shift();
+
+    while (this.queue2.length > 0) {
+        this.queue1.push(this.queue2.shift());
+    }
+
+    return popValue;
+};
+
+/**
+ * Get the top element.
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    while (this.queue1.length > 1) {
+        this.queue2.push(this.queue1.shift());
+    }
+    return this.queue1[0];
+};
+
+/**
+ * Returns whether the stack is empty.
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.queue1.length == 0 && this.queue2.length == 0;
+};
+/**方法二 用一个Queue就可以实现
  * Initialize your data structure here.
  */
 var MyStack = function() {

f6a58ae2846afd4eabc7040a6c7cb3f6f08ad280
pingw206 <pingw206@gmail.com>
2021-01-31 16:08:48 +0800

    3rd update

--- leetcode/S0100_SameTree.js
+++ leetcode/S0100_SameTree.js
@@ -8,5 +8,5 @@ var isSameTree = function(p, q) {
     return (p.val == q.val && isSameTree(p.left,q.left) && isSameTree(p.right, q.right)); 
 }
  
-// 我对于迭代还是不清楚，导致因为没有写p.val == q.val这个判断条件耽误了太长时间做这道题，40分钟？？
-// f(n-1)只是指它下面的都可以用这个函数，但是这个层级本身也是需要判断的呀！
+// 2021-1-31 没有写p.val == q.val这个判断条件, f(n-1)只是指它下面的都可以用这个函数来判断，但是这个层级本身也是需要判断的呀！
+// 而且最后一句， 是直接return 里面含true或false了，不要只返回true丢了false的情况

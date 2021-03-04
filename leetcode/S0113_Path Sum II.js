c7563eba006529057a452acd5b5e4a7e95ed3ab1
pingw206 <pingw206@gmail.com>
2021-03-04 21:41:39 +0800

    init

---
+++ leetcode/S0113_Path Sum II.js
@@ -0,0 +1,32 @@
+/** 2021-3-4 
+ * 这道题我开始没想到里面函数的入参写哪些，参考了一下入参就写出来了
+ * 
+ */
+var pathSum = function(root, targetSum) {
+  var result = [];
+  var path = [];
+  findPath(root, targetSum, result, path, 0);
+  return result;
+};
+
+var findPath = function(root, targetSum, result, path, sum) {
+  if (root == null) {
+      return;
+  }
+  sum += root.val;
+  path.push(root.val);
+  if (root.left == null && root.right == null) {
+      // sum is the sum of path  
+      /** 或者我把sum写到了这里面,会比外面那层相比少一些，只有全部路径下的的几个sum
+          path.forEach(function(item) {
+            sum += item;
+          })
+       */
+      if (sum == targetSum) {
+          result.push(path.map((x)=>x));
+      }
+  }
+  findPath(root.left, targetSum, result, path, sum);
+  findPath(root.right, targetSum, result, path, sum);
+  path.pop();
+}

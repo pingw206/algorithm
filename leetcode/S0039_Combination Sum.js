e9bf232b7996f58ee6075e131dbbe1570cda0746
pingw206 <pingw206@gmail.com>
2021-03-14 22:40:32 +0800

    init

---
+++ leetcode/S0039_Combination Sum.js
@@ -0,0 +1,27 @@
+/** 2021-3-11 
+ * 跟77题的区别是，每个数字可以用多次，画树形图的时候注意，往下可以重复写一个数字，但是横向不要重复，当垂直加起来超过7时停止
+ * 其实也是硬币组合问题，问最多的组合数，用DP解法，让求所有的组合，用DFS回溯法 
+ */
+
+ 
+
+var combinationSum = function(candidates, target) {
+  var paths = [];
+  var path = [];
+  dfs(0, candidates, target, path, paths);
+  return paths;
+};
+
+var dfs = function(start, candidates, target, path, paths) {
+  if (target <= 0) {
+      return;
+  }
+  for (var i = start; i < candidates.length; i++) {
+      path.push(candidates[i]);
+      if (target == candidates[i]) {
+          paths.push(path.map((x)=>x)); 
+      }
+      dfs(i, candidates, target-candidates[i], path, paths); //上一层是几，这层还是从数字几开始，可重复
+      path.pop();
+  }
+}
--- leetcode/S0077. Combinations.js
+++ leetcode/S0077. Combinations.js
@@ -1,13 +1,36 @@
 /** 2021-3-11
- *  跟17题相比，因为是k个 1-n的数字的组合，所以多了个start，标记从几到n
- * 学会画树形图 
- *   1             2          3
+ *  跟17题相比，因为是k个 1～n的数字的组合，所以多了个start，标记从几到n
+ * 学会画树形图 k=2，n=4，注意不重复的，到3就停止，因为要满足深度是2，
+ *    1            2          3
  * 2  3  4       3   4        4
-  3 4 4          4
  * 
  */
 
- //方法二  
+ //方法一更好一些,给k逐渐减少数字
+ var combine = function(n, k) {
+  var nums = new Array(n).fill(0);
+  nums.forEach(function(num, index) {
+      nums[index] = index+1;
+  });   //把1～n写进数组里，方便操作[1,2,3,4]
+  var paths = [];
+  var path = [];
+  dfs(0, nums, k, paths, path);
+  return paths;
+};
+var dfs = function(start, nums, k, paths, path) {
+  if (k == 0) { //没有数字了
+      return;   
+  }
+  for (var i = start; i <= nums.length-k; i++) { //因为nums的index是从0开始，所以start=0，i=nums.len-k
+      path.push(nums[i]);
+      if (k - 1 == 0) { // or path.length == k
+          paths.push(path.map((x)=>x));
+      }
+      dfs(i+1, nums, k-1, paths, path); //用完一个数，k-1
+      path.pop();
+  }
+}
+ //方法二 广度范围不太好理解 
  var combine = function(n, k) {  //n=4,k=2
   var paths = [];
   var path = [];
@@ -22,8 +45,7 @@ var dfs = function(depth, start, paths, path, n, k) {
   if (depth > k) {  //跳出条件，组合不能多于K个长度--深度
       return;
   }
-  for (var i = start; i <= n+depth-k; i++) {  //---广度，n-i+1(范围) > k-depth（个数） 推理出来的
-      //console.log(i); 1234-234-34
+  for (var i = start; i <= n+depth-k; i++) {  //---广度，已经有的个数（dep-1）+ (max...n)本层最大数max到n的个数 =k总个数 即 （dep-1)+(n-max+1)=k 即max = n+dep-k 推理出来的；其实也可以i<=n,可以通过，只不过白多走一些步骤
       path.push(i);
       if (depth == k) {  //路尽头标志
           paths.push(path.map((x)=>x));

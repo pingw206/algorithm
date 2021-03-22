47c7880e889585cadcedee9608eb5e8e7b89be41
pingw206 <pingw206@gmail.com>
2021-03-22 22:31:16 +0800

    2nd update

--- leetcode/S0331_Verify Preorder Serialization of a Binary Tree.js
+++ leetcode/S0331_Verify Preorder Serialization of a Binary Tree.js
@@ -1,5 +1,6 @@
-/**2020-1-11 我完全没有想到怎么来判断，消消乐原理：最小子树 a # # 这样肯定是前序遍历； 
+/**2021-1-11 我完全没有想到怎么来判断，消消乐原理：最小子树 a # # 这样肯定是前序遍历； 
  * 关键词：迭代
+ * 2021-3-22 如果最小子树是，那么把最小子树去掉后，还应该是前序遍历，所以就依次去掉是前序的树，最后只剩下一个树，还是前序
  */
 /**
  * @param {string} preorder
@@ -15,13 +16,12 @@ var isValidSerialization = function(preorder) {
              stack[stack.length-1] == "#" &&
              stack[stack.length-2] == "#") {
           // if the parent node is already resolved, then return false
-          if (stack[stack.length-3] == "#") {  //这个情况是 ### 没必要存在的情况。这种case没跑通，发现的特别判断
-              return false;
+          if (stack[stack.length-3] == "#") {  //这个情况是 因为长度为3时， 边界case ### 是一种没必要存在的情况, 如 “###”， 或"1,#,#,#,#"
+             return false;
           }
           stack.pop(); stack.pop(); stack.pop();
           stack.push("#");
       }
   }
-  console.log(stack);
   return stack.length == 1 && stack[0] == "#";
 };

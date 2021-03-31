c163973368c0006baca96e9e66b797ae256fe584
pingw206 <pingw206@gmail.com>
2021-03-31 22:33:02 +0800

    2nd update

--- leetcode/S0063_Unique Paths II.js
+++ leetcode/S0063_Unique Paths II.js
@@ -1,4 +1,15 @@
-/*2021-2-15 我有一点点想到了障碍怎么处理，就是一个点遇到由障碍组成时，那么要不算障碍的方法，也就是0的处理 */
+/*2021-2-15 ｜ 3-31
+* 障碍怎么处理，就是一个点遇到由障碍组成时，要break，还有初始化0的这点，就是为了给遇到break后，其他没走的地方都赋值0，只需要给首行和首列这样操作，因为就是一条路走到黑，如果障碍在图中间，还有别的路
+* 一个很容易错的地方，就是初始化的时候,没有写break，只是赋值，这样就导致不能通过的例子如[[1,0]]应该输出0结果输出1，因为前面遇到障碍，后面还能赋1，其实后面都走不通了，不break的话，再后面还能利用前面的值（1）
+if (i == 0 || j == 0) {
+    if (obstacleGrid[i][j] == 1) {
+        dpTable[i][j] = 0;
+    } else {
+        dpTable[i][j] = 1;
+    }
+}
+*
+*/
 
 var uniquePathsWithObstacles = function(obstacleGrid) {
   var m = obstacleGrid.length; //行数
@@ -12,7 +23,7 @@ var uniquePathsWithObstacles = function(obstacleGrid) {
   //initialize the boundary of dp table 第一行和第一列的方法填1处理
   for (var j=0; j<n; j++) {
       if (obstacleGrid[0][j] == 1) {
-          break;
+          break;  //break 是循环结束， continue才是跳过本轮循环；也就是说如果遇到j=1的时候是障碍，那么j>=1后面的路都是0种，不通！
       }
       dpTable[0][j] = 1;
   }

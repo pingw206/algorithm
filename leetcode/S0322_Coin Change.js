fbe375969fd036b4beaf7beb0d828f2c82735885
pingw206 <pingw206@gmail.com>
2021-04-05 18:22:25 +0800

    2nd update

--- leetcode/S0322_Coin Change.js
+++ leetcode/S0322_Coin Change.js
@@ -14,13 +14,14 @@ var coinChange = function(coins, amount) {
   //dp Progress
   for (var i=1; i<=amount; i++) {
     var minNum = -1; // default value: -1, means i cannot be made up,无法加和得出时的返回值
-    for (var j=0;coins[j] <= i && j<coins.length;j++) {
+    for (var j=0;coins[j] <= i && j<coins.length;j++) {  //i是总钱数，单个币要比总钱数小 && 币在coins范围内
       // the sub problem should be valid
-      if (dpTable[i-coins[j]] != -1) {
+      if (dpTable[i-coins[j]] != -1) {  //-1表示没有组合的可能，那么minNum还是-1，不需要重新赋值，进行下一个j
         minNum = (minNum == -1) ? dpTable[i-coins[j]]+1 : Math.min(minNum, dpTable[i-coins[j]]+1);
+        //minNum还是-1的话，递推+1就是组合数，不是-1已经赋值过了，就要取一个最少数组合
       }
     }
     dpTable[i] = minNum;
   }
-  return dpTable[dpTable.length-1];
+  return dpTable[dpTable.length-1];  //或dpTable[amount];
 };

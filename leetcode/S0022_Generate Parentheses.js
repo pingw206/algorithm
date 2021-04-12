5a2982b093fe5df9b0df425e0c0ed178aba97c4b
pingw206 <pingw206@gmail.com>
2021-04-12 16:09:03 +0800

    init

---
+++ leetcode/S0022_Generate Parentheses.js
@@ -0,0 +1,33 @@
+/** 2021-4-12
+ *  想不明白可以在debug里面运行一下看看， 想明白条件是关键
+ * 条件是剩余的括号数，也就是还没使用的
+ * 可以push “(” 的条件是，还有剩余的“(” ---- 此时不管“)”，会在后面的条件中做限制判断
+ *  可以push “)” 的条件是，剩余的“)”要比“(”多 ---- 也就是用上的右括号比左括号少，还可以push右
+ */
+var generateParenthesis = function(n) {
+    
+  var result = [];
+  var path = [];
+  
+  dfs(n, n, path, result);
+  return result;
+};
+
+var dfs = function(leftRemain, rightRemain, path, result) {
+  if (leftRemain == 0 && rightRemain == 0) {
+      result.push(path.join(""));
+  }
+
+  if (leftRemain > 0) {
+      path.push("(");
+      dfs(leftRemain-1, rightRemain, path, result);
+      path.pop();
+  }
+  if (leftRemain < rightRemain) {
+      path.push(")");
+      dfs(leftRemain, rightRemain-1, path, result);
+      path.pop();
+  }
+}
+
+generateParenthesis(3)

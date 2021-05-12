ae6ce5715e98e6fbdad73e8551db2fb29838f485
pingw206 <pingw206@gmail.com>
2021-05-12 22:42:58 +0800

    2nd update

--- leetcode/S0437_Path Sum III.js
+++ leetcode/S0437_Path Sum III.js
@@ -2,11 +2,13 @@
  * 不用从头出发，所以每一层都可以重新往下走，也就是有两个地方有root.left, root.right的入口
  * 判断符合时也不用判断叶子节点，直接等于targetSum就行
  * 
+ * 第一 calcSumNum是sum题常规写法，第二 因为从每个节点都可以再出发去计算跟下面节点的sum，所以要从这个节点开始遍历，选择一种遍历方法即可，比如这里的前序遍历，preOrder， 
+ * 
  */
 
-var pathSum = function(root, sum) {
+var pathSum = function(root, targetSum) {
   var num = new Object({"key":0});
-  preOrder(root, sum, num);
+  preOrder(root, targetSum, num);
   return num["key"];
 };
 
@@ -14,9 +16,9 @@ var preOrder = function(root, targetSum, num) {
   if (root == null) {
       return;
   }
-  calcSumNum(root, 0, targetSum, num);//第一层root都走完后
-  preOrder(root.left, targetSum, num);//第二层root.left往下走
-  preOrder(root.right, targetSum, num)
+  calcSumNum(root, 0, targetSum, num);//第一层root都走完后--前序遍历访问这个root
+  preOrder(root.left, targetSum, num);//第二层root.left往下走---前序遍历左节点
+  preOrder(root.right, targetSum, num) //前序遍历右节点
 }
 
 var calcSumNum = function(root, sum, targetSum, num) {  //从root节点一直走下去加一起
@@ -24,7 +26,7 @@ var calcSumNum = function(root, sum, targetSum, num) {  //从root节点一直走
       return;
   }
   sum += root.val;
-  if (sum == targetSum) { //遇到target就加1，但是不返回，继续走
+  if (sum == targetSum) { //遇到target就加1，表示有了一种组合方法; 不会返回，继续算下去，sum可能会比target大，但是因为节点有负数的存在，所以还有可能再出现合适的sum的
       num["key"]++;
   }
   calcSumNum(root.left, sum, targetSum, num);

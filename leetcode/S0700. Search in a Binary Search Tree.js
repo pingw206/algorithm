40c43155f1495886d23c8c413eb8097821da45ba
pingw206 <pingw206@gmail.com>
2021-05-23 22:30:22 +0800

    init

---
+++ leetcode/S0700. Search in a Binary Search Tree.js
@@ -0,0 +1,30 @@
+/** 2021-5-23
+ *  我竟然就差没想到while条件里面写什么。。。
+ */
+// iterative solution
+var searchBST = function(root, val) {
+  while (root != null) {
+      if (root.val < val) {
+          root = root.right;
+      } else if (root.val > val) {
+          root = root.left;
+      } else {
+          return root;
+      }
+  }
+  return null;
+};
+
+// resursive solution
+var searchBST = function(root, val) {
+  if (root == null) {
+      return null;
+  }
+  if (root.val < val) {
+      return searchBST(root.right, val);
+  } else if (root.val > val) {
+      return searchBST(root.left, val);
+  } else {
+      return root;
+  }
+};

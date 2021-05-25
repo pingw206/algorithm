f4d96d82a5a5fb998744ee8f6b41e0ef0ed7df9b
pingw206 <pingw206@gmail.com>
2021-05-25 22:20:28 +0800

    3rd update

--- leetcode/S0092_ReverseLinkedList_II.js
+++ leetcode/S0092_ReverseLinkedList_II.js
@@ -1,31 +1,39 @@
-//真的是要被绕哭，头插法在中间，还没有搞一个新链表出来
-    // [1, end1] -> [start2, end2] -> [start3, length]
-    //   ==> 
-    // [1, end1] -> [end2, start2] -> [start3, length]
-//2020-12-28 想明白哪个是在新链表上生成，哪个是在原链表上走（或是需要的链表上走），有点难以理解，cursorNode一直在新链表之外需要的链表上
-var reverseBetween = function(head, m, n) {
-  if (head == null) {
-      return null;
-  }
-  var newList = new ListNode(0);
-  newList.next = head;
-  var end1 = newList;
-  for (var i = 1; i < m; i++) {
-      end1 = end1.next;
-  }
-  var start2 = end1.next;
-  end1.next = null;
-  
-  var cursorNode = start2;
-  for (var i = m; i <= n; i++) {
-      var nextCursorNode = cursorNode.next;
-      
-      var tempNode = end1.next;
-      end1.next = cursorNode;
-      cursorNode.next = tempNode;
-      
-      cursorNode = nextCursorNode;
-  }
-  start2.next = cursorNode;
-  return newList.next;  
+//2020-12-28 | 2021-5-25
+/* 分三段， 关键是中间这段 
+	 [1, end1] -> [start2, end2] -> [start3, length]
+       ==> 
+   [1, end1] -> [end2, start2] -> [start3, length]
+*/ 
+var reverseBetween = function(head, left, right) {
+    if (head == null) {
+        return null;
+    }
+    var newList = new ListNode(0);
+    newList.next = head;
+    //1.走到end1
+    var end1 = newList;
+    for (var i=1; i<left;i++) {
+        end1 = end1.next;
+    }
+   
+    //2.中间段用头插法来翻转 
+    var start2 = end1.next;
+    end1.next = null; //end1断开后面，这样才可以当做一段没有尾巴的数组来翻转头插
+
+    var cursorNode = start2;
+    for (i=left;i<=right;i++) {
+        var headNode = cursorNode;
+        cursorNode = cursorNode.next;
+
+       /*  var tempNode = end1.next;
+        end1.next = headNode;
+        headNode.next = tempNode; */
+
+        headNode.next = end1.next;
+        end1.next = headNode;
+
+    }
+    //3.start2接start3
+    start2.next = cursorNode; 
+    return newList.next;
 };

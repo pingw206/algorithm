d255703c97c3c09d372daae7bb84e5cd424c1147
pingw206 <pingw206@gmail.com>
2021-05-30 22:48:08 +0800

    update 迭代写法

--- leetcode/S0094.BinaryTreeInorderTraversal.js
+++ leetcode/S0094.BinaryTreeInorderTraversal.js
@@ -1,3 +1,6 @@
+/**2020-5-1 ｜2021-5-30*/
+
+//方法一 递归解法
 var inorderTraversal = function(root) {
   var arr = [];
   myInorder(root, arr);
@@ -11,3 +14,28 @@ var myInorder = function(root, arr) {
   arr.push(root.val);
   myInorder(root.right, arr);
 }
+
+//方法二 迭代写法 ：如何把递归翻译成迭代
+// DFS: using a stack to simulate the recursive process
+var inorderTraversal = function(root) {
+  if (root == null) {
+      return [];    
+  }
+  var result = [];
+  var stack = [];
+  while (root != null) {
+      stack.push(root);
+      root = root.left;
+  }
+  while (stack.length != 0) {
+      var topNode = stack[stack.length-1];
+      stack.pop();
+      result.push(topNode.val);
+      var rightNode = topNode.right;
+      while (rightNode != null) {
+          stack.push(rightNode);
+          rightNode = rightNode.left;
+      }
+  }
+  return result;
+};

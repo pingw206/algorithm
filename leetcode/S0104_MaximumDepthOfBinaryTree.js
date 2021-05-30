4978a784c03c707ad9814213763ea596e1908c32
pingw206 <pingw206@gmail.com>
2021-05-30 22:52:55 +0800

    更新BFS

--- leetcode/S0104_MaximumDepthOfBinaryTree.js
+++ leetcode/S0104_MaximumDepthOfBinaryTree.js
@@ -1,13 +1,37 @@
 //2021-1-30 不需要再额外构建一个函数来递归；实在是没有想到，实质还是从头往下递归的思想，上面是左右两个深度再加一，这样的话最上面就是0
+//方法一 DFS
 var maxDepth = function(root) {
   if (root == null) {
       return 0;
   }
-  var leftDep = maxDepth(root.left);
-  var rightDep = maxDepth(root.right);
-  return Math.max(leftDep, rightDep) + 1;
+  var childrenDepth = Math.max(maxDepth(root.left), maxDepth(root.right));
+  return childrenDepth + 1;
 };
-//下面构造一个树，来看stack运行
+
+//方法二 
+// BFS
+var maxDepth = function(root) {
+  if (root == null) { return 0; }
+      
+  var queue = [];
+  queue.push(root);
+  var height = 0;
+  while (queue.length != 0) {
+      var curLevelSize = queue.length;
+      for (var i = 0; i < curLevelSize; i++) {
+          var node = queue.shift();
+          if (node.left != null) {
+              queue.push(node.left);
+          }
+          if (node.right != null) {
+              queue.push(node.right);
+          }
+      }
+      height++;
+  }
+  return height;
+};
+//解析：下面构造一个树，来看stack运行
 // [1,2,3,n,n,6,7]
 var root = new TreeNode(1);
 root.left = new TreeNode(2);

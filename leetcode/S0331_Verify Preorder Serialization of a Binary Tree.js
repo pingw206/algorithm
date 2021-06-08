6bd32a831903dbb58f7e9e6fae34f47d62beccd4
pingw206 <pingw206@gmail.com>
2021-06-08 22:17:17 +0800

    update

--- leetcode/S0331_Verify Preorder Serialization of a Binary Tree.js
+++ leetcode/S0331_Verify Preorder Serialization of a Binary Tree.js
@@ -1,13 +1,15 @@
-/**2021-1-11 我完全没有想到怎么来判断，消消乐原理：最小子树 a # # 这样肯定是前序遍历； 
+/**2021-1-11 | 2021-3-22 | 6-8
+我完全想不到怎么来判断， 所以这种题就只能记住解决思路
  * 关键词：迭代
- * 2021-3-22 如果最小子树是，那么把最小子树去掉后，还应该是前序遍历，所以就依次去掉是前序的树，最后只剩下一个树，还是前序
+消消乐原理：最小子树 a # # 这样肯定是前序遍历；
+ *  如果最小子树是，那么把最小子树去掉后，还应该是前序遍历，所以就依次去掉是前序的树，最后只剩下一个树，还是前序
  */
 /**
  * @param {string} preorder
  * @return {boolean}
  */
 
-var isValidSerialization = function(preorder) {
+ var isValidSerialization = function(preorder) {
   var preorderArray = preorder.split(",");
   var stack = new Array();
   for (var i = 0; i < preorderArray.length; i++) {
@@ -16,7 +18,8 @@ var isValidSerialization = function(preorder) {
              stack[stack.length-1] == "#" &&
              stack[stack.length-2] == "#") {
           // if the parent node is already resolved, then return false
-          if (stack[stack.length-3] == "#") {  //这个情况是 因为长度为3时， 边界case ### 是一种没必要存在的情况, 如 “###”， 或"1,#,#,#,#"
+					//这个情况是 因为长度为3时， 边界case ### 是一种没必要存在的情况, 如 “###”， 或"1,#,#,#,#"
+          if (stack[stack.length-3] == "#") { 
              return false;
           }
           stack.pop(); stack.pop(); stack.pop();
